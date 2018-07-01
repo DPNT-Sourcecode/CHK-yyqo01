7de3ae85841a642ad78c8ed88b23547cb81c3ed3
sbx_user1060 <sbx_user1060@ip-10-12-60-19.eu-west-2.compute.internal>
2018-07-01 16:57:35 +0000

    Sun Jul 01 16:57:35 UTC 2018

---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+import unittest
+from nose_parameterized import parameterized
+
+from solutions.CHK import checkout_solution
+
+
+class TestSum(unittest.TestCase):
+    @parameterized.expand([('e',),(2,), ('&',), ('£',)])
+    def test_invalid_input(self, fail_char):
+        self.assertEqual(
+            checkout_solution.checkout(fail_char), -1)
+        
+    def test_chk_special_offers(self):
+        self.assertEqual(
+            checkout_solution.checkout('AAABBCD'), 
+            210)
+
+
+if __name__ == '__main__':
+    unittest.main()
