2607a434f6ef2344d1d397af1dd28157a8214004
sbx_user1060 <sbx_user1060@ip-10-12-60-19.eu-west-2.compute.internal>
2018-07-01 17:03:35 +0000

    Sun Jul 01 17:03:35 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,17 @@
 
+def valid_input(chars):
+    """
+    Check input is of type string and limited to characters:
+    A B C D
+    """
+    if not isinstance(chars, str):
+        return False
+    
+    for c in chars:
+        if c not in ['A', 'B', 'C', 'D']:
+            return False
 
+    return False
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
