1a4941835cbf0a06e472af7e26fbedffe8e1819f
sbx_user1060 <sbx_user1060@ip-10-12-60-19.eu-west-2.compute.internal>
2018-07-01 19:10:04 +0000

    Sun Jul 01 19:10:04 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -185,7 +185,6 @@ def get_free_offer(items, item, offer):
     amount = 0
 
     if items[item] >= offer['num']:
-        import pdb;pdb.set_trace()
         free_item_key = offer['value']
         required_items = get_required_offer_items(item, offer)
         free_items = items[item] / required_items
@@ -204,6 +203,8 @@ def calculate_special_offers(items, total):
     # Get offers largest to smallest savings and apply them
     for offer in sorted(SPECIAL_OFFERS, key=lambda x: x['saving'], reverse=True):
         item = offer['target']
+        if item == 'H':
+            import pdb;pdb.set_trace()
         if offer.get('type', '') == 'discount':
             total += discount_offer(items, item, offer)
         if offer.get('type', '') == 'free':
