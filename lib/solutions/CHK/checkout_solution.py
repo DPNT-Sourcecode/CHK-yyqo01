bbc653823f057567a437636aeefe40a91c7f8fe8
sbx_user1060 <sbx_user1060@ip-10-12-60-19.eu-west-2.compute.internal>
2018-07-01 17:09:35 +0000

    Sun Jul 01 17:09:35 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -40,5 +40,28 @@ def checkout(skus):
     if not valid_input:
         return -1
     
+    items = {
+        'A': 0,
+        'B': 0,
+        'C': 0,
+        'D': 0
+    }
+
+    # Calculate skus found
+    for char in skus:
+        items[char] += 1
+    
     total = 0
+
+    # Add special offers to total and remove items in offers
+    for item in SPECIAL_OFFERS:
+        if items[item] > SPECIAL_OFFERS[item]['num']:
+            offer_count = items[item] / SPECIAL_OFFERS[item]['num']
+            items[item] = items[item] % SPECIAL_OFFERS[item]['num']
+            total = offer_count * SPECIAL_OFFERS['amount']
+    
+    for item in items:
+        total += items[item] * SKU[item]
+    
+    return total
 
